f1da45f4bdccda530b390b1db5aac875589df8df
Hugo Mota <hugomota@Hugos-Mac-mini.home>
2017-05-14 17:42:00 +0100

    First commit

---
+++ applications/brew-cask-script.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+# homebrew taps
+brew tap caskroom/cask
+brew tap caskroom/versions
+brew tap caskroom/fonts
+
+# Updated grep
+brew install grep
+
+# Updated bash
+brew install bash
+
+# homebrew apps
+brew install brew-cask
+brew install git
+brew install git-extras
+brew install wget
+
+# Updated python/pip
+brew install python
+pip install --upgrade setuptools
+pip install --upgrade pip
+
+# cask apps
+brew cask install atom-beta
+brew cask install sublime-text
+brew cask install vlc
+brew cask install dropbox
+brew cask install cakebrew
+brew cask install slack
+brew cask install appcleaner
+brew cask install firefox
+brew cask install sketch
+brew cask install skype
+brew cask install iterm2-beta
+brew cask install alfred
+brew cask install google-chrome
+brew cask install cyberduck
+brew cask install devdogs
+brew cask install cakebrew
+brew cask install spectacle
+brew cask install caffeine
+brew cask install sketch-toolbox
+brew cask install imageoptim
+brew cask install firefox
+brew cask install jumpcut
+brew cask install transmission
+
+# Install zsh
+brew install zsh
+pip chsh -s /usr/local/bin/zsh
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+
+pip install mackup
+
+sudo mdimport /Applications
+
+# clean
+brew cask cleanup
+
+# verify
+brew doctor
